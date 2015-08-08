14d5843c9bfec110f5c436b3cf6e2e923de065e0
Daniel Oram <danieloramnz@gmail.com>
2015-08-08 17:08:44 +1200

    implement CSV handler
    implement class for CSV handler and create a csv reader that parses the
    relevant data to an array of model objects.

---
+++ RentalsAKL/RentalsAKL/CSV/RentDataModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RentalsAKL.CSV
+{
+    public class RentDataModel
+    {
+
+        public string region { get; set; }
+        public string marketRentArea { get; set; }
+        public string propertyType { get; set; }
+        public string bedrooms { get; set; }
+        public float averageRentPrice { get; set; }
+    }
+}
